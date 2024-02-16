99fd84561d7aec8923c4d0d484b53b24864609c1
Marcin Białecki <79873623+MartinBialecki@users.noreply.github.com>
2024-02-16 13:00:54 +0100

    Add REM sleep phase detection and eye movement detection test.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "BluetoothSerial.h"
 #include <Adafruit_MPU6050.h>
 
-//comment for a diode with a common cathode
-//#define COMMON_ANODE
+// comment for a diode with a common cathode
+// #define COMMON_ANODE
 
 #define BUTTON_PIN 39
 #define RED_LED_PIN 23
@@ -23,6 +23,14 @@
 
 int leftEyeSensorValue = 0;
 int rightEyeSensorValue = 0;
+int lastLeftEyeSensorValue = 0;
+int lastRightEyeSensorValue = 0;
+int maxDeviation = 0;                            // największe chwilowe odchylenie z 2 czujników odbiciowych od poprzednich ich wartości
+int detectionSensitive = 50;                     // od tej wartości zależy przy jakim odchyleniu od ostatnio zapisanego pomiaru będzie zapisywać się następny dodając też ilość ruchów oczu
+int numberOfEyeMovementsToActivatePerMinute = 5; // ilosc wykrytych ruchów gałek ocznych na minutę, które będą miały oznaczać wykrycie fazy snu REM
+int numberOfEyeMovementsLeftSensor = 0;
+int numberOfEyeMovementsRightSensor = 0;
+int lastTimeReduction = 0;
 
 BluetoothSerial SerialBT;
 // MPU6050 object
@@ -33,16 +41,21 @@ const float GRAVITY_SCALE_FACTOR = 9.81;
 uint8_t lastButtonState = HIGH;
 unsigned long pressStartTime = 0;
 unsigned long pressDuration = 0;
-//unsigned long shortPressTime = 0;
+// unsigned long shortPressTime = 0;
 unsigned long longPressTime = 2000;
 bool shortPressExecuted = false;
 bool longPressExecuted = false;
 bool taskExecuted = true;
 
-//czas startu nadawania sygnałów pomagających w uświadomieniu się we śnie w milisekundach (dodawanie milisekund na podstawie funkcji "millis()")
-unsigned long signalsStartTime = 0;
-float initialDelayTimeInMinutes = 1; //początkowe opóźnienie nadawania sygnałów w minutach
-float additionalDelayTimeInMinutes = 10; //dodatkowe opóźnienie nadawania sygnałów w minutach
+bool REMdetected = false;                      // flaga wykrycia fazy REM, jeśli wartość to "true", to nadawane są sygnały mające na celu pomóc uświadomić się we śnie
+unsigned long REMdetectionStartTime = 0;       // czas startu wykrywania fazy REM
+float initialDelayTimeInMinutes = 1;           // początkowe opóźnienie wykrywania fazy REM w minutach
+float additionalDelayTimeInMinutes = 10;       // dodatkowe opóźnienie wykrywania fazy REM w minutach
+float delayTimeAfterREMdetectionInMinutes = 5; // opóźnienie wykrywania fazy REM w minutach po jej wykryciu
+int numberOfSignals = 20;                      // ilość nadawanych sygnałów po wykryciu fazy REM
+float signalsCounter = 0;                      // zlicza nadane już sygnały w obecnym cyklu nadawania
+int signalLength = 500;                        // długość pojedynczego sygnału w milisekundach
+int signalTimer = 0;                           // przechowuje czas do następnego sygnału, pozwala uruchamiać sygnał w określonych odstępach czasu bez wstrzymywania działania urządzenia
 
 bool isLedOn = false;
 uint8_t ledBrightness = 10;
@@ -73,15 +86,20 @@ int batteryLevel(uint8_t batteryLevelPin);
 void buttonFunctions(int number);
 int batteryLevel(uint8_t batteryLevelPin);
 bool devicePositionAndTemperature();
+void numberOfEyeMovementsRedution();
+void numberOfEyeMovementsAddition();
+void sendingSignals();
+void shortPressButtonTask();
+void longPressButtonTask();
 
 void setup()
 {
-  //reduce CPU frequency to 80 MHz form default 240 MHz (reduce power consumption from 63 mA to 30 mA)
+  // reduce CPU frequency to 80 MHz form default 240 MHz (reduce power consumption from 63 mA to 30 mA)
   setCpuFrequencyMhz(80);
 
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, LOW);
-  
+
   pinMode(BUTTON_PIN, INPUT);
   pinMode(LEFT_EYE_SENSOR, INPUT);
   pinMode(RIGHT_EYE_SENSOR, INPUT);
@@ -93,7 +111,7 @@ void setup()
   digitalWrite(IR_DIOD_AND_ACCELEROMETER_POWER, HIGH);
   pinMode(VIBRATION_MOTOR, OUTPUT);
   digitalWrite(VIBRATION_MOTOR, LOW);
-  
+
   ledcSetup(ledChannel[0], freq, resolution);
   ledcAttachPin(RED_LED_PIN, ledChannel[0]);
   ledcSetup(ledChannel[1], freq, resolution);
@@ -111,8 +129,8 @@ void setup()
   Serial.println("%");
   SerialBT.print(batteryValue);
   SerialBT.println("%");
-  
-  if(batteryValue < 2)
+
+  if (batteryValue < 2)
   {
     changeLedState(1);
     delay(300);
@@ -143,11 +161,12 @@ void setup()
   Wire.begin(I2C_SDA, I2C_SCL);
 
   // Inicjalizacja MPU6050
-  if (!mpu.begin()) {
-  Serial.println("Nie znaleziono układu MPU6050. Sprawdź połączenia!");
+  if (!mpu.begin())
+  {
+    Serial.println("Nie znaleziono układu MPU6050. Sprawdź połączenia!");
   }
 
-  signalsStartTime = millis() + (initialDelayTimeInMinutes * 60 * 1000);
+  REMdetectionStartTime = millis() + (initialDelayTimeInMinutes * 60 * 1000);
 
   // devicePositionAndTemperature();
 }
@@ -156,8 +175,12 @@ void loop()
 {
   readSerialData();
   buttonPressTaskExecutor();
-  
-  
+
+  numberOfEyeMovementsRedution();
+  numberOfEyeMovementsAddition();
+
+  sendingSignals();
+
   // if (isButtonChangedState(BUTTON_PIN, lastButtonState) && lastButtonState == LOW)
   // {
   //   if(functionNumber >= 4)
@@ -170,101 +193,55 @@ void loop()
 
   // if(isLedOn && ledBrightnessWithAccelerometer)
   //   executLedBrightnes(ledBrightness);
-
-  // leftEyeSensorValue = analogRead(LEFT_EYE_SENSOR);
-  // Serial.print("L: ");
-  // Serial.println(leftEyeSensorValue);
-  // SerialBT.print("L: ");
-  // SerialBT.println(leftEyeSensorValue);
-
-  // rightEyeSensorValue = analogRead(RIGHT_EYE_SENSOR);
-  // Serial.print("R: ");
-  // Serial.println(rightEyeSensorValue);
-  // SerialBT.print("R: ");
-  // SerialBT.println(rightEyeSensorValue);
-  // delay(500);
-  // Serial.println((analogRead(BATTERY_LEVEL_PIN) / (4096 / 3.3)) * 2);
-  // SerialBT.println((analogRead(BATTERY_LEVEL_PIN) / (4096 / 3.3)) * 2);
-  // batteryValue = batteryLevel(BATTERY_LEVEL_PIN);
-  // SerialBT.println(batteryValue);
-  // Serial.println(batteryValue);
-  // delay(500);
-  // isButtonChangedState(YELLOW_BUTTON, lastStateYellowButton);
-  // if (lastStateYellowButton == LOW && isLedOn)
-  // {
-  //   setLedBrightness(ledBrightness - 1);
-  //   delay(10);
-  // }
 }
-
 
 void buttonPressTaskExecutor()
 {
   ButtonChangedStateMonitor(BUTTON_PIN, lastButtonState);
 
-  if(longPressExecuted == true)
+  if (longPressExecuted == true)
   {
     taskExecuted = true;
-    signalsStartTime = millis() + (initialDelayTimeInMinutes * 60 * 1000);
-    changeLedState(1);
+    longPressButtonTask();
   }
 
-  if(shortPressExecuted == true && taskExecuted == false)
+  if (shortPressExecuted == true && taskExecuted == false)
   {
     taskExecuted = true;
-    if(signalsStartTime > millis()) {
-      signalsStartTime += (additionalDelayTimeInMinutes * 60 * 1000);
-    }
-    else {
-      signalsStartTime = millis() + (additionalDelayTimeInMinutes * 60 * 1000);
-    }
-
-    digitalWrite(BUZZER, HIGH);
-    changeLedState(1);
-    delay(20);
-    digitalWrite(BUZZER, LOW);
-    delay(130);
-    changeLedState(0);
-
-    batteryValue = batteryLevel(BATTERY_LEVEL_PIN);
-    Serial.print(batteryValue);
-    Serial.println("%");
-    SerialBT.print(batteryValue);
-    SerialBT.println("%");
-
-    Serial.print("Czas do rozpoczęcia monitorowania ruchów gałek ocznych w minutach: ");
-    Serial.println((signalsStartTime - millis())/ 60000);
-    SerialBT.print("Czas do rozpoczęcia monitorowania ruchów gałek ocznych w minutach: ");
-    SerialBT.println((signalsStartTime - millis())/ 60000);
+    shortPressButtonTask();
   }
 }
 
 bool ButtonChangedStateMonitor(int button, uint8_t &lastState)
 {
-  if(lastState == LOW) {
-    if (pressStartTime == 0) {
+  if (lastState == LOW)
+  {
+    if (pressStartTime == 0)
+    {
       pressStartTime = millis();
       taskExecuted = false;
       longPressExecuted = false;
       shortPressExecuted = false;
-    } else {
-      pressDuration = millis() - pressStartTime;
     }
-  } else {
-    if(taskExecuted == false) {
-      if(pressDuration >= longPressTime) {
+    else
+      pressDuration = millis() - pressStartTime;
+  }
+  else
+  {
+    if (taskExecuted == false)
+    {
+      if (pressDuration >= longPressTime)
         longPressExecuted = true;
-      }
-      else {
+      else
         shortPressExecuted = true;
-      }
     }
     // Zresetuj licznik czasu jeśli przycisk nie jest wciśnięty
     pressStartTime = 0;
     pressDuration = 0;
   }
 
-  if(longPressExecuted == false && pressDuration >= longPressTime) {
+  if (longPressExecuted == false && pressDuration >= longPressTime)
+  {
     longPressExecuted = true;
   }
 
@@ -313,11 +290,11 @@ void setLedBrightness(uint8_t value, bool showResult)
 
 void executLedBrightnes(uint8_t ledBrightness)
 {
-  #ifdef COMMON_ANODE
-    ledBrightness = 255 - ledBrightness;
-  #endif
+#ifdef COMMON_ANODE
+  ledBrightness = 255 - ledBrightness;
+#endif
 
-  if(ledBrightnessWithAccelerometer)
+  if (ledBrightnessWithAccelerometer)
   {
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
@@ -326,10 +303,10 @@ void executLedBrightnes(uint8_t ledBrightness)
     ledcWrite(ledChannel[1], ledBrightness * (std::abs(a.acceleration.y) / 11));
     ledcWrite(ledChannel[2], ledBrightness * (std::abs(a.acceleration.z) / 11));
   }
-  else 
+  else
   {
-    for(int i = 0; i <= 2; i++)
-    ledcWrite(ledChannel[i], ledBrightness);
+    for (int i = 0; i <= 2; i++)
+      ledcWrite(ledChannel[i], ledBrightness);
   }
 }
 
@@ -397,7 +374,7 @@ bool exeCmd()
       {
         SerialBT.println("Nieprawidłowa wartość komendy 'LED'.");
         return false;
-      } 
+      }
     }
     else if (strncmp(query, "VIB", 3) == 0)
     {
@@ -411,7 +388,7 @@ bool exeCmd()
       {
         SerialBT.println("Nieprawidłowa wartość komendy 'VIB'.");
         return false;
-      } 
+      }
     }
     else if (strncmp(query, "BUZ", 3) == 0)
     {
@@ -425,7 +402,7 @@ bool exeCmd()
       {
         SerialBT.println("Nieprawidłowa wartość komendy 'BUZ'.");
         return false;
-      } 
+      }
     }
     else if (strncmp(query, "GYR", 3) == 0)
     {
@@ -439,13 +416,13 @@ bool exeCmd()
       {
         SerialBT.println("Nieprawidłowa wartość komendy 'GYR'.");
         return false;
-      } 
+      }
     }
     else if (strncmp(query, "ALL", 3) == 0)
     {
       if (intQueryValue > 256)
       {
-        if(isLedOn || digitalRead(BUZZER) || digitalRead(VIBRATION_MOTOR))
+        if (isLedOn || digitalRead(BUZZER) || digitalRead(VIBRATION_MOTOR))
         {
           changeLedState(0);
           digitalWrite(VIBRATION_MOTOR, LOW);
@@ -462,7 +439,7 @@ bool exeCmd()
       {
         SerialBT.println("Nieprawidłowa wartość komendy 'ALL'.");
         return false;
-      } 
+      }
     }
     else
     {
@@ -489,24 +466,24 @@ void buttonFunctions(int number)
   digitalWrite(BUZZER, LOW);
   digitalWrite(VIBRATION_MOTOR, LOW);
 
-  switch(number)
+  switch (number)
   {
-    case 1:
-      digitalWrite(VIBRATION_MOTOR, HIGH);
-      delay(100);
-      tone(BUZZER, 1000, 500);
-      break;
-    case 2:
-      changeLedState(1);
-      break;
-    case 3:
-      digitalWrite(BUZZER, HIGH);
-      break;
-    case 4:
-      digitalWrite(VIBRATION_MOTOR, HIGH);
-      break;
-    default:
-      break;
+  case 1:
+    digitalWrite(VIBRATION_MOTOR, HIGH);
+    delay(100);
+    tone(BUZZER, 1000, 500);
+    break;
+  case 2:
+    changeLedState(1);
+    break;
+  case 3:
+    digitalWrite(BUZZER, HIGH);
+    break;
+  case 4:
+    digitalWrite(VIBRATION_MOTOR, HIGH);
+    break;
+  default:
+    break;
   }
 
   batteryValue = batteryLevel(BATTERY_LEVEL_PIN);
@@ -520,28 +497,28 @@ void buttonFunctions(int number)
 int batteryLevel(uint8_t batteryLevelPin)
 {
   // int calibration = 217; //(before the changes) ((0.35 * (4096 / 3.3)) / 2); calibration due to differences in readings when measuring with a multimeter, the difference was about 0.36 volts
-  int calibration = 87; //((0.14 * (4096 / 3.3)) / 2); calibration due to differences in readings when measuring with a multimeter, the difference was about 0.36 volts
+  int calibration = 87;  //((0.14 * (4096 / 3.3)) / 2); calibration due to differences in readings when measuring with a multimeter, the difference was about 0.36 volts
   int batteryMax = 2606; //((4.2 * 0.5)*(4096 / 3.3))
   int batteryMin = 2172; //((3.5 * 0.5)*(4096 / 3.3))
   float onePercentOfBattery = (batteryMax - batteryMin) / 100.0;
   int sum = 0;
   int batteryLevelPinValue;
 
-  for(int i = 0; i < 20; i++)
+  for (int i = 0; i < 20; i++)
   {
     sum += analogRead(batteryLevelPin);
     delay(5);
   }
 
   batteryLevelPinValue = (sum / 20) + calibration;
-  //Serial.println(batteryLevelPinValue);
+  // Serial.println(batteryLevelPinValue);
   Serial.print((batteryLevelPinValue / (4096 / 3.3)) * 2);
   Serial.print("V - ");
   SerialBT.print((batteryLevelPinValue / (4096 / 3.3)) * 2);
   SerialBT.print("V - ");
-  if(batteryLevelPinValue < batteryMin + onePercentOfBattery)
+  if (batteryLevelPinValue < batteryMin + onePercentOfBattery)
     return 1;
-  else if(batteryLevelPinValue > batteryMax - onePercentOfBattery)
+  else if (batteryLevelPinValue > batteryMax - onePercentOfBattery)
     return 100;
   else
     return (batteryLevelPinValue - batteryMin) / onePercentOfBattery;
@@ -552,7 +529,7 @@ bool devicePositionAndTemperature()
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-   // Wypisanie wartości na port szeregowy
+  // Wypisanie wartości na port szeregowy
   Serial.print("Akcelerometr (m/s^2): x = ");
   Serial.print(a.acceleration.x);
   Serial.print(", y = ");
@@ -574,4 +551,131 @@ bool devicePositionAndTemperature()
   SerialBT.println("C.");
 
   return true;
+}
+
+void numberOfEyeMovementsRedution()
+{
+  if ((lastTimeReduction + (60000 / numberOfEyeMovementsToActivatePerMinute)) <= millis())
+  {
+    if (numberOfEyeMovementsLeftSensor > 0)
+      numberOfEyeMovementsLeftSensor -= 1;
+    if (numberOfEyeMovementsRightSensor > 0)
+      numberOfEyeMovementsRightSensor -= 1;
+
+    if ((lastTimeReduction + (2 * (60000 / numberOfEyeMovementsToActivatePerMinute))) < millis())
+      lastTimeReduction = millis();
+    else
+      lastTimeReduction += (60000 / numberOfEyeMovementsToActivatePerMinute);
+  }
+}
+
+void numberOfEyeMovementsAddition()
+{
+  if (REMdetectionStartTime <= millis())
+  {
+    leftEyeSensorValue = analogRead(LEFT_EYE_SENSOR);
+    rightEyeSensorValue = analogRead(RIGHT_EYE_SENSOR);
+
+    if (abs(leftEyeSensorValue - lastLeftEyeSensorValue) >= detectionSensitive)
+    {
+      numberOfEyeMovementsLeftSensor += 1;
+      lastLeftEyeSensorValue = leftEyeSensorValue;
+    }
+    if (abs(rightEyeSensorValue - lastRightEyeSensorValue) >= detectionSensitive)
+    {
+      numberOfEyeMovementsRightSensor += 1;
+      lastRightEyeSensorValue = RightEyeSensorValue;
+    }
+
+    if (numberOfEyeMovementsLeftSensor >= numberOfEyeMovementsToActivatePerMinute || numberOfEyeMovementsRightSensor >= numberOfEyeMovementsToActivatePerMinute)
+    {
+      REMdetectionStartTime = millis() + (delayTimeAfterREMdetectionInMinutes * 60000);
+      REMdetected = true;
+    }
+    else
+      REMdetectionStartTime = millis() + 100;
+  }
+}
+
+void sendingSignals()
+{
+  if (REMdetected)
+  {
+    if (signalTimer <= millis())
+    {
+      changeLedState(-1);
+      signalsCounter += 0.5;
+      signalTimer = millis() + signalLength;
+    }
+
+    if (signalsCounter >= numberOfSignals)
+    {
+      changeLedState(0);
+      REMdetection = false;
+    }
+  }
+  else
+    signalsCounter = 0;
+}
+
+void shortPressButtonTask()
+{
+  if (REMdetectionStartTime > millis())
+    REMdetectionStartTime += (additionalDelayTimeInMinutes * 60 * 1000);
+  else
+    REMdetectionStartTime = millis() + (additionalDelayTimeInMinutes * 60 * 1000);
+
+  digitalWrite(BUZZER, HIGH);
+  digitalWrite(VIBRATION_MOTOR, HIGH);
+  changeLedState(1);
+  delay(20);
+  digitalWrite(BUZZER, LOW);
+  delay(30);
+  digitalWrite(VIBRATION_MOTOR, LOW);
+  delay(100);
+  changeLedState(0);
+
+  batteryValue = batteryLevel(BATTERY_LEVEL_PIN);
+  Serial.print(batteryValue);
+  Serial.println("%");
+  SerialBT.print(batteryValue);
+  SerialBT.println("%");
+
+  Serial.print("Czas do rozpoczęcia monitorowania ruchów gałek ocznych w minutach: ");
+  Serial.println((REMdetectionStartTime - millis()) / 60000);
+  SerialBT.print("Czas do rozpoczęcia monitorowania ruchów gałek ocznych w minutach: ");
+  SerialBT.println((REMdetectionStartTime - millis()) / 60000);
+}
+
+void longPressButtonTask()
+{
+  REMdetectionStartTime = millis() + (initialDelayTimeInMinutes * 60 * 1000);
+
+  leftEyeSensorValue = analogRead(LEFT_EYE_SENSOR);
+  rightEyeSensorValue = analogRead(RIGHT_EYE_SENSOR);
+
+  if (abs(leftEyeSensorValue - lastLeftEyeSensorValue) >= abs(rightEyeSensorValue - lastRightEyeSensorValue))
+    maxDeviation = abs(leftEyeSensorValue - lastLeftEyeSensorValue);
+  else
+    maxDeviation = abs(rightEyeSensorValue - lastRightEyeSensorValue);
+
+  if (maxDeviation >= (detectionSensitive / 5))
+  {
+    lastLeftEyeSensorValue = leftEyeSensorValue;
+    lastRightEyeSensorValue = rightEyeSensorValue;
+
+    Serial.print("Odnotowane odchylenie: ");
+    Serial.println(maxDeviation);
+    SerialBT.print("Odnotowane odchylenie: ");
+    SerialBT.println(maxDeviation);
+
+    digitalWrite(BUZZER, HIGH);
+    if (maxDeviation < detectionSensitive)
+      delay(200 * (maxDeviation / detectionSensitive));
+    else
+      delay(200);
+    digitalWrite(BUZZER, LOW);
+  }
+
+  delay(20);
 }
